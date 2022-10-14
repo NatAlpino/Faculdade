2cbbe7c9bd99ac9cd39ee872ed90359300a49288
Nat Alpino <nathyalpino@hotmail.com>
2022-10-14 07:23:03 -0300

    chore: reorg folders

---
+++ Exercicios/Media | Mediana/media-mediana.js
@@ -0,0 +1,13 @@
+/*
+A média é o resultado da soma de todos os números do conjunto dividida pela quantidade de números que foram somados. 
+Já a mediana será o número que ocupa a posição central do conjunto, considerando que o conjunto esteja crescentemente ordenado.
+_______
+
+Para o cálculo da mediana, há uma particularidade que deve ser observada. Se a quantidade de números do conjunto for ímpar, 
+a mediana será o elemento do meio do vetor. 
+Por exemplo, para o vetor [21, 24, 31, 32, 32, 32, 33, 44, 65], a mediana será o número 32, que está bem no meio do vetor. 
+
+Porém, se a quantidade de número do conjunto for par, então a mediana será a média aritmética dos dois valores centrais.
+Por exemplo, para o vetor [18, 19, 19, 22, 44, 45, 46, 46, 47, 48], a mediana será (44 + 45) / 2 → 89 /2, que é igual a 44,5. 
+Além do cálculo, seu programa deverá exibir os resultados na tela.
+*/
